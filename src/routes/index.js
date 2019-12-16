53166a3d910c37b8c1d692f25fbc4fccb50de148
Ruan Michel Adabo <ruanbento@gmail.com>
2019-12-16 18:25:23 -0300

    [UPDATE] Ajuste das rotas para as requisições AJAX
    Rotas novas:
    city - responsável por verificar se a cidade pesquisada ja esta
    armazenada em nosso banco de dados.
    getTopResults - responsável por pegar as 5 cidades mais pesquisadas.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const openWeatherMap = require('./../providers/open-weather-map.js')
+const database = require('./../providers/database.js');
 
 router.get('/', (req, res, next) => {
     res.render('index');
 });
 
 router.get('/search', (req, res, next) => {
-    res.send({errors: 'Digite o nome de uma cidade!'});
+    res.status(400).send({errors: 'Digite o nome de uma cidade!'});
 });
 
 router.get('/search/:cityName', (req, res, next) => {
-    console.log('looking for: ', req.params.cityName);
     openWeatherMap.getCurrentWeatherByCityName(req.params.cityName).then((data) => {
         if(data.cod && data.cod === '404') {
-            res.send({errors: 'Cidade não encontrada!'});
+            res.status(404).send({errors: 'Cidade não encontrada!'});
         }
-        res.send(data);
+        res.status(200).send(data);
     }).catch(function (err) {
-        res.send(err);
+        res.status(500).send(err);
     });
+});
+
+router.get('/getTopResults', (req, res, next) => {
+    database.getTopResults().then((data) => {
+        res.status(200).send(data);
+    }).catch(function (err) {
+        res.status(404).send(err);
+    });
+});
+
+router.get('/city/:id', (req, res, next) => {
+    openWeatherMap.getCurrentWeatherByCityId(req.params.id).then((data) => {
+        if(data.cod && data.cod === '404') {
+            res.status(404).send({errors: 'Cidade não encontrada!'});
+        }
+        database.setCity(data).then((data) => {
+            res.status(200).send(data);
+        }).catch(function (err) {
+            res.status(404).send(err);
+        });
+    }).catch(function (err) {
+        res.status(500).send(err);
+    });
+
 });
 
 module.exports = router;
