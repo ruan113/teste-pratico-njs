afd942f7cbfb75c6ff5aaf844b8b29c784f36401
Ruan Michel Adabo <ruanbento@gmail.com>
2019-12-16 05:33:42 -0300

    [UPDATE] Ajustes na interface temporaria
    Ajustes possibilitaram utilização de funções jquery e ajax

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
 const router = express.Router();
+const openWeatherMap = require('./../providers/open-weather-map.js')
 
 router.get('/', (req, res, next) => {
     res.render('index');
+});
+
+router.get('/search', (req, res, next) => {
+    res.send({errors: 'Digite o nome de uma cidade!'});
+});
+
+router.get('/search/:cityName', (req, res, next) => {
+    console.log('looking for: ', req.params.cityName);
+    openWeatherMap.getCurrentWeatherByCityName(req.params.cityName).then((data) => {
+        if(data.cod && data.cod === '404') {
+            res.send({errors: 'Cidade não encontrada!'});
+        }
+        res.send(data);
+    }).catch(function (err) {
+        res.send(err);
+    });
 });
 
 module.exports = router;
